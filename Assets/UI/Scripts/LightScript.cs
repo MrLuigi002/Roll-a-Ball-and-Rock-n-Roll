2e643413e2ab80ca7e3473cad73f3ecb6b73717f
MrLuigi002 <mrluigi002@gmail.com>
2019-11-09 15:35:30 +0100

    Made the base of the Main menu

---
+++ Assets/UI/Scripts/LightScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    Animator anim;
+    bool lightState = false;
+    //int lightHash = Animator.StringToHash("Switch");
+
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        lightState = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && lightState == false)
+        {
+            lightState = true;
+            anim.SetTrigger ("Switch");
+        }
+    }
+}
